0c703f97cfc4b3aee90bca91bec2acfba03dcbe5
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-10-13 15:25:08 +0200

    Optimize circuit with pulp, adding integral constraints
    Add also SNI constraint: no path from input to output

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@
 import numpy as np
 import networkx as nx
 import scipy.optimize
+import pulp
 
 nodes = list(range(15))
 SNI_nodes = set([3, 8, 10, ]) # and 13
@@ -50,37 +51,34 @@ def flat_paths(g):
 def opt_setup(g):
     all_paths = flat_paths(g)
     n_paths = sum(len(p) for p in all_paths.values())
-    edges = g.edges
+    edges = list(g.edges)
     e_ids = {e: i for i, e in enumerate(edges)}
-    A_ub11 = np.zeros((len(all_paths), n_paths))
-    A_ub12 = np.zeros((len(all_paths), len(edges)))
-    b_ub1 = np.zeros((len(all_paths),))
 
-    A_ub21 = np.zeros((n_paths, n_paths)) # identity matrix
-    A_ub22 = np.zeros((n_paths, len(edges)))
-    b_ub2 = np.zeros((n_paths,)) # zero vector
-    print('n_paths', n_paths)
-    print('len(edges)', len(edges))
-    print('len(all_apths)', len(all_paths))
-
+    prob = pulp.LpProblem("edges", pulp.LpMinimize)
+    ecuts = pulp.LpVariable.dicts("ecut", range(len(edges)), 0, 1, pulp.LpInteger)
+    pcuts = pulp.LpVariable.dicts("pcut", range(n_paths), 0, 1, pulp.LpInteger) # int ?
+    prob += sum(ecuts.values()), "number of cut edges"
+    paths_sets_ids = {}
     for path_id, (path_set_id, path) in enumerate([(path_set_id, path) for path_set_id, ((src, dest), paths) in enumerate(all_paths.items()) for path in paths]):
-        A_ub11[path_set_id,path_id] = -1
-        A_ub21[path_id, path_id] = 1
-        for edge in path:
-            e_id = e_ids[edge]
-            A_ub22[path_id,e_id] = -1
-    for path_set_id, ((src, dest), paths) in enumerate(all_paths.items()):
-        b_ub1[path_set_id] = -(len(paths) - 1)
+        prob += pcuts[path_id] <= sum(ecuts[e_ids[edge]] for edge in path), "path is cut if at least one edge is cut " + str(path_id)
+        paths_sets_ids.setdefault(path_set_id, []).append(path_id)
+    for path_set_id, path_ids in paths_sets_ids.items():
+        prob += sum(pcuts[path_id] for path_id in path_ids) >= len(path_ids) - 1, "Cut at least all but one paths " + str(path_set_id)
+    # cut "pass-through" paths to get SNI
+    for src in g.nodes:
+        if g.nodes.data()[src].get('IN'):
+            for dest in g.nodes:
+                if g.nodes.data()[dest].get('OUT'):
+                    for i, path in enumerate(all_paths[(src, dest)]):
+                        prob += sum(ecuts[e_ids[edge]] for edge in path) >= 1, "pass-through path {} from {} to {}".format(i, src, dest)
 
-    A_ub = np.concatenate((np.concatenate((A_ub11, A_ub12), axis=1), np.concatenate((A_ub21, A_ub22), axis=1)), axis=0)
-    print('b_ub1', b_ub1)
-    print('b_ub2', b_ub2)
-    b_ub = np.concatenate((b_ub1, b_ub2), axis=0)
-    c = np.concatenate((np.zeros((n_paths,)), np.ones((len(edges),))), axis=0)
-    bounds = (0, 1)
-    opt = scipy.optimize.linprog(c, A_ub, b_ub, bounds=bounds)
-    print(repr(opt))
-    print(opt.x)
+
+    # TODO: add SNI: cut all in-out paths
+    #prob.writeLP('aes.lp')
+    prob.solve()
+    print("Status:", pulp.LpStatus[prob.status])
+    print("Cut edges:", [edges[i] for i, e in ecuts.items() if pulp.value(e) == 1.0])
+    #print("Res:", [(edges[i], pulp.value(e)) for i, e in ecuts.items()])
 
 
 
@@ -133,14 +131,14 @@ def test():
     #cut_SNI(gaes)
     simplify_graph(gaes)
     import pprint
-    pprint.pprint(compute_paths(gaes))
+    #pprint.pprint(compute_paths(gaes))
     #import matplotlib.pyplot as plt
     #nx.draw(gaes, with_labels=True)
     #plt.show()
     print('NI:', test_graph_NI(gaes))
     print('SNI:', test_graph_SNI(gaes))
     print('edges', gaes.edges)
-    print('flat_paths', flat_paths(gaes))
+    #print('flat_paths', flat_paths(gaes))
     opt_setup(gaes)
 
 if __name__ == '__main__':
